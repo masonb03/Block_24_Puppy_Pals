d30c4e52a8e6109585ee4d769a4d57594c3641e6
Mason <bevinsm355@gmail.com>
2025-03-25 21:46:32 -0400

    Created a good looking code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import {puppyList} from './data.js'
+import {useState} from 'react'
 
 function App() {
+const [puppies, setPuppies] = useState(puppyList)
+console.log("puppyList: ", puppyList);
 
   return (
-    <>
-      
-    </>
+    <div className="App">
+      {
+        puppies.map((puppy) =>{
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
+    </div>
   )
 }
 
