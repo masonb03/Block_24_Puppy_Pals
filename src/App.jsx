5065fe4f57cb0c765750969d2c2b89dbb2517da1
Mason <bevinsm355@gmail.com>
2025-03-27 17:20:20 -0400

    created a functinig code that displays teh information and is stylized a little bit

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,38 @@ import {useState} from 'react'
 
 function App() {
 const [puppies, setPuppies] = useState(puppyList)
+const [featPupId, setFeatPupId] = useState(null)
 console.log("puppyList: ", puppyList);
 
-  return (
-    <div className="App">
-      {
-        puppies.map((puppy) =>{
-          return <p key={puppy.id}>{puppy.name}</p>
-        })
-      }
-    </div>
-  )
+const handleClick = (puppy) => {
+  console.log("puppy id: ", puppy.id);
+  setFeatPupId(puppy.id);
+};
+
+const featuredPup = puppies.find((pup) => pup.id === featPupId)
+console.log("Featured Puppy: ", featuredPup);
+
+return (
+  <div className="App">
+    {puppies.map((puppy) => {
+      return (
+        <p onClick={() => handleClick(puppy)} key={puppy.id}>
+          {puppy.name}
+        </p>
+      );
+    })}
+    {featPupId && (
+        <div className="featured">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+
+  </div>
+);
 }
 
 export default App
